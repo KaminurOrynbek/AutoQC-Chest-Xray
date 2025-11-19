427945bca8eb295bbd7b2cb56d194cd329b4e5f0
KaminurOrynbek <231189@astanait.edu.kz>
2025-11-20 04:15:29 +0500

    fixed

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,16 +1,21 @@
 from app.models.users import User
 from sqlmodel import Session, select
+from typing import List, Optional
 
 class UserRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_by_username(self, username: str) -> User | None:
+    def get_by_username(self, username: str) -> Optional[User]:
         return self.session.exec(select(User).where(User.username == username)).first()
 
-    def get_first_user(self) -> User | None:
+    def get_first_user(self) -> Optional[User]:
         """Возвращает любого пользователя, если есть"""
         return self.session.exec(select(User)).first()
+
+    def get_all(self) -> List[User]:
+        """Возвращает список всех пользователей"""
+        return self.session.exec(select(User)).all()
 
     def add(self, user: User) -> User:
         self.session.add(user)
